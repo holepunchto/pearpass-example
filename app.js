233213b436b597e86c1289b4521029c76350dd21
supersuryaansh <prod.genx@gmail.com>
2024-10-01 15:12:14 +0530

    Added Pear

---
+++ app.js
@@ -0,0 +1,281 @@
+// Design API for pear processes here
+/** @typedef {import('pear-interface')} */ /* global Pear */
+
+// This live reloads the app during development
+// Import necessary modules
+import Corestore from 'corestore'
+import Pearwords from './pearwords.js'
+import b4a from 'b4a'
+import fs from 'fs'
+// For pop-ups
+import Swal from 'sweetalert2'
+Pear.updates(() => Pear.reload())
+Pear.teardown(() => pearwords.close())
+
+// Path to store autobase
+const baseDir = Pear.config.storage + '/store'
+console.log(baseDir)
+// Initialise global variables here
+let pearwords
+
+// Create an autobase if one does not exist already
+async function createBase () {
+  // Don't create the base if already exists
+  if (fs.existsSync(baseDir)) {
+    pearwords = new Pearwords(new Corestore(baseDir))
+    console.log('Base exists')
+  } else {
+    console.log('Base does not exist, prompt user to create a new one')
+
+    const result = await Swal.fire({
+      title: 'No Vault found',
+      showDenyButton: true,
+      confirmButtonText: 'Create New Vault',
+      denyButtonText: 'Load a Vault',
+      allowEscapeKey: false,
+      backdrop: `
+      rgba(33, 33, 38, 1)
+        left top
+        no-repeat
+      `,
+      allowOutsideClick: false
+    })
+
+    if (result.isConfirmed) {
+      // Logic for creating a new vault
+      pearwords = new Pearwords(new Corestore(baseDir))
+      await Swal.fire('New vault created!', '', 'success')
+      createTable()
+    } else if (result.isDenied) {
+      // Load an existing vault
+      const vaultKeyResult = await Swal.fire({
+        title: 'Enter Your Vault Key',
+        input: 'text',
+        backdrop: `
+        rgba(33, 33, 38, 1)
+          left top
+          no-repeat
+        `,
+        inputAttributes: {
+          autocapitalize: 'off'
+        },
+        showCancelButton: true,
+        confirmButtonText: 'Create',
+        showLoaderOnConfirm: true,
+        inputValidator: (value) => {
+          if (!value) {
+            return 'Key can not be blank!'
+          }
+        },
+        preConfirm: async (vaultKey) => {
+          try {
+            pearwords = new Pearwords(new Corestore(baseDir), vaultKey)
+          } catch (error) {
+            Swal.showValidationMessage(`Error: ${error}`)
+          }
+        },
+        allowOutsideClick: () => !Swal.isLoading()
+      })
+
+      if (vaultKeyResult.isConfirmed) {
+        await Swal.fire('Loaded an existing vault', '', 'info')
+        createTable()
+      } else {
+        // An empty store folder is created due to failure, delete it and start over
+        fs.rmSync(baseDir, { recursive: true, force: true })
+        await createBase()
+      }
+    }
+  }
+
+  await pearwords.ready()
+  console.log('Ready to use pearwords')
+
+  // Add bootstrap key
+  setKey()
+
+  await pearwords.replicate()
+  console.log('Replication started')
+}
+
+function setKey () {
+  document.querySelector('.session-link > p').innerHTML = b4a.toString(
+    pearwords.bootstrapKey(),
+    'hex'
+  )
+}
+
+// Push data to the html table
+function push (username, password, website) {
+  // Get the table by its ID
+  const table = document.getElementById('passwordTable')
+
+  // Create a new row and its cells
+  const newRow = table.insertRow()
+  const usernameCell = newRow.insertCell(0)
+  const passwordCell = newRow.insertCell(1)
+  const websiteCell = newRow.insertCell(2)
+
+  // Assign the values to the cells
+  usernameCell.textContent = username
+  passwordCell.textContent = password
+  websiteCell.textContent = website
+}
+
+// Clean the table of all records, will be used for re-rendering
+function cleanTable () {
+  const table = document.getElementById('passwordTable')
+
+  // Remove all rows except the header (first row)
+  while (table.rows.length > 1) {
+    table.deleteRow(1) // Keep deleting the second row until only the header remains
+  }
+}
+
+// Show data from the base to frontend
+async function createTable () {
+  cleanTable()
+  for await (const data of pearwords.list()) {
+    push(data.value[0], data.value[1], data.value[2])
+  }
+}
+
+// Call this to start base creation
+await createBase()
+
+// Listen and add new passwords to the list
+pearwords.base.view.core.on('append', (e) => {
+  console.log('exexex')
+  createTable()
+})
+
+// Check for base writable state
+pearwords.base.on('writable', (e) => {
+  console.log('I am writable')
+})
+
+// Create the initial table
+createTable()
+
+// Destroy the base
+document.querySelector('.destroy-session').addEventListener('click', (e) => {
+  if (
+    confirm(
+      'You will lose complete access to this vault and your passwords. Continue?'
+    )
+  ) {
+    if (fs.existsSync(baseDir)) {
+      fs.rmSync(baseDir, { recursive: true, force: true })
+      console.log('Destroying Base')
+      createBase()
+    } else {
+      console.error('Base does not exist')
+    }
+  } else {
+    console.log('Destroy dialogue cancelled')
+  }
+})
+
+// Copy Pearwords key to the clipboard
+document.querySelector('.session-link').addEventListener('click', (e) => {
+  // Use the Clipboard API
+  navigator.clipboard
+    .writeText(b4a.toString(pearwords.bootstrapKey(), 'hex'))
+    .then(() => {
+      console.log('Key copied to clipboard')
+      alert('Key copied to clipboard!')
+    })
+    .catch((err) => {
+      console.error('Error copying string: ', err)
+    })
+})
+
+// Add data to the autobase
+document.querySelector('.add-data').addEventListener('click', async (e) => {
+  // Ask user if the data type if a note or a password
+  const { value: dataType } = await Swal.fire({
+    title: 'Add new data',
+    input: 'select',
+    inputOptions: {
+      password: 'Password',
+      note: 'Note'
+    },
+    inputPlaceholder: 'Select data type',
+    showCancelButton: true,
+    inputValidator: (value) => {
+      return new Promise((resolve) => {
+        if (value === 'password') {
+          resolve()
+        } else if (value === 'note') {
+          resolve()
+        } else {
+          resolve('You need to select an option :)')
+        }
+      })
+    }
+  })
+  if (dataType) {
+    // Difference between data types here
+    if (dataType === 'password') {
+      // Create form for password field inputs
+      const { value: formValues } = await Swal.fire({
+        title: 'New Password',
+        html: `
+        <label for="swal-username">Username:</label>
+        <input id="swal-username" class="swal2-input" placeholder="Username" >
+
+        <label for="swal-password">Password:</label>
+        <input id="swal-password" class="swal2-input" placeholder="Password" >
+
+        <label for="swal-website">Website:</label>
+        <input id="swal-website" class="swal2-input" placeholder="Website" >
+
+        `,
+        focusConfirm: false,
+        preConfirm: () => {
+          // Return input from these fields
+          return [
+            document.getElementById('swal-username').value,
+            document.getElementById('swal-password').value,
+            document.getElementById('swal-website').value
+          ]
+        }
+      })
+      if (formValues) {
+        await pearwords.add(formValues[0], formValues)
+        createTable()
+      }
+    } else {
+      Swal.fire('You selected a note')
+    }
+  }
+})
+
+// Handle the add writer function
+document.querySelector('.add-writer').addEventListener('click', async (e) => {
+  // Check if the button is disabled
+  if (e.target.hasAttribute('disabled')) {
+    return
+  }
+  // Add new writer pop-up
+  await Swal.fire({
+    title: 'Add a writer',
+    input: 'text',
+    showCancelButton: true,
+    inputValidator: (key) => {
+      if (!key) {
+        return 'You need to add a key'
+      }
+    },
+    preConfirm: async (key) => {
+      try {
+        await pearwords.addWriter(key)
+        Swal.fire('Added a new writer')
+      } catch (error) {
+        Swal.showValidationMessage(`Error: ${error}`)
+      }
+    }
+  })
+})
+
+console.log(b4a.toString(pearwords.writerKey(), 'hex'))
---
+++ pearwords.js
@@ -0,0 +1,146 @@
+// the js module powering the mobile and desktop app
+
+import Autobase from 'autobase'
+import Hyperbee from 'hyperbee'
+import Hyperswarm from 'hyperswarm'
+import b4a from 'b4a'
+
+class Pearwords {
+  constructor (corestore, key) {
+    this.corestore = corestore
+    // Initialise the base
+    this.base = new Autobase(corestore, key, {
+      valueEncoding: 'json',
+      open (store) {
+        return new Hyperbee(store.get('view'), {
+          extension: false,
+          keyEncoding: 'utf-8',
+          valueEncoding: 'json'
+        })
+      },
+      // New data blocks will be added using the apply function
+      async apply (nodes, view, base) {
+        for (const node of nodes) {
+          const op = node.value
+          // Add support for adding other peers as a writer to the base
+          if (op.type === 'addWriter') {
+            await base.addWriter(b4a.from(op.key, 'hex'))
+          } else if (op.type === 'addRecord') {
+            // This adds a new record
+            await view.put(op.key, op.value)
+          } else if (op.type === 'removeRecord') {
+            // Remove an existing record
+            await view.del(op.key)
+          }
+        }
+      }
+    })
+
+    // Create new Hyperswarm to replicate
+    this.swarm = null
+  }
+
+  // Check if base is ready
+  async ready () {
+    return this.base.ready()
+  }
+
+  // Close the base
+  async close () {
+    if (this.swarm) {
+      await this.swarm.destroy()
+    }
+    await this.base.close()
+  }
+
+  // Need this key to become a writer
+  writerKey () {
+    return this.base.local.key
+  }
+
+  // Return bootstrap key of the base
+  // This is what other peers should use to bootstrap the base from
+  bootstrapKey () {
+    return this.base.key
+  }
+
+  // This is used for hyperswarm join and discovery
+  // Needed to replicate base across peers
+  discoveryKey () {
+    return this.base.discoveryKey
+  }
+
+  // Get data of all indexes in the base
+  list (opts) {
+    return this.base.view.createReadStream(opts)
+  }
+
+  // Get data stored in a specific key
+  async get (key) {
+    const node = await this.base.view.get(key)
+    if (node === null) return null
+    return node.value
+  }
+
+  // Add a peer as a writer
+  async addWriter (key) {
+    try {
+      await this.base.append({
+        type: 'addWriter',
+        key: b4a.isBuffer(key) ? b4a.toString(key, 'hex') : key
+      })
+    } catch (error) {
+      throw error
+    }
+
+    return true
+  }
+
+  // To later add removeWriter
+  async removeWriter (key) {}
+
+  // Check if the base is writable
+  isWritable () {
+    return this.base.writable
+  }
+
+  // Start Replicating the base across peers
+  async replicate () {
+    await this.ready()
+    this.swarm = new Hyperswarm({
+      keyPair: await this.corestore.createKeyPair('hyperswarm')
+    })
+    // Join swarm over discovery key
+    const discovery = this.swarm.join(this.discoveryKey())
+
+    // Listen for connections
+    this.swarm.on('connection', (connection, peerInfo) => {
+      console.log('\rPeer joined: ', b4a.toString(peerInfo.publicKey, 'hex'))
+      // Replicate the base
+      this.base.replicate(connection)
+    })
+  }
+
+  // Append a key/value to the base
+  async add (key, value) {
+    await this.base.append({
+      type: 'addRecord',
+      key,
+      value
+    })
+
+    console.log('Data added')
+  }
+
+  // Remove a key pair
+  async remove (key) {
+    await this.base.append({
+      type: 'removeRecord',
+      key,
+      value: null
+    })
+  }
+}
+
+// module.exports = Pearwords;
+export default Pearwords
